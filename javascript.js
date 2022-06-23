9b2f77505f10ed6c3811f20c8f21d9c1f3e88c3e
WendelFiti <wendel_miranda10@hotmail.com>
2022-06-23 10:43:09 -0400

    First Commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+    function imgSlider(anything){
+        document.querySelector('.monster').src = anything
+    }
+
+    function changeBgcolor(color){
+        const sec = document.querySelector('.sec')
+        sec.style.background = color;
+    }
+
+
+
